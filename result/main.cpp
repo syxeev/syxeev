7c337c7bfcf63bf68e43ea9f937595e6f51d556c
ka904001 <44923555+ka904001@users.noreply.github.com>
2019-10-02 15:55:19 +0300

    Create main.cpp

---
+++ result/main.cpp
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "headr_ss.h"
+#include "headr_gg.h"
+#include <iostream>
+using namespace std;
+
+int func(gg & D, ss & Q){
+	int max = D.S;
+	if ( Q.D > max) {
+		max = Q.D;
+	}
+	if ( Q.Q > max) {
+		max = Q.Q;
+	}
+	return max;
+}
+int main()
+{
+	int P;
+	cin >> P;
+	gg ob1{P};
+	ss ob2;
+	int a, b;
+	cin >>a>>b;
+	ob2.input(a, b);
+	int max = func(ob1, ob2);
+	cout << "max = " << max;
+	return(0);
+}
